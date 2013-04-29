c0ef153866422128eb741bf02e55ed9808f7ceae
Andrew Davidson <aadavids@uwaterloo.ca>
2013-04-28 20:30:41 -0400

    add Josh's initial work

---
+++ src/server_battle/Battle.java
@@ -0,0 +1,8 @@
+package server_battle;
+
+import java.util.*;
+
+public class Battle {
+	Map<Integer,String> unitTable=new HashMap<Integer, String>();
+	Map<Position,String> unitPositionTable=new HashMap<Position, String>();
+}
---
+++ src/server_battle/Position.java
@@ -0,0 +1,46 @@
+package server_battle;
+
+public class Position {
+	private final Integer x;
+	private final Integer y;
+
+	public Position (Integer X, Integer Y) {
+		this.x = X;
+		this.y = Y;
+	}
+	
+    public int hashCode() {
+    	int hashFirst = x != null ? x.hashCode() : 0;
+    	int hashSecond = y != null ? y.hashCode() : 0;
+
+    	return (hashFirst + hashSecond) * hashSecond + hashFirst;
+    }
+
+	public boolean equals(Object other) {
+		if (other instanceof Position) {
+			Position otherPosition = (Position) other;
+			return 
+					((  this.x == otherPosition.y ||
+					( this.x != null && otherPosition.x != null &&
+					this.x.equals(otherPosition.x))) &&
+					(	this.y == otherPosition.y ||
+					( this.y != null && otherPosition.y != null &&
+					this.y.equals(otherPosition.y))) );
+		}
+
+		return false;
+	}
+
+	public String toString()
+	{ 
+		return "(" + x + ", " + y + ")"; 
+	}
+
+	public Integer getx() {
+		return x;
+	}
+
+	public Integer gety() {
+		return y;
+	}
+}
