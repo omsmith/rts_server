256fbecfaf925348ddeb0c6a564b7debf9ae0de9
Josh Mackenzie <jnmacken@uwaterloo.ca>
2013-04-29 07:50:00 -0400

    Finished with Unit Location and Selection for now
    Implemented storing unit location as well as looking it up via a single
    unit selection or a box selection.

--- src/server_battle/Battle.java
+++ src/server_battle/Battle.java
@@ -3,6 +3,5 @@ package server_battle;
 import java.util.*;
 
 public class Battle {
-	Map<Integer,String> unitTable=new HashMap<Integer, String>();
-	Map<Position,String> unitPositionTable=new HashMap<Position, String>();
+	Unit_LocationandSelection Units = new Unit_LocationandSelection();
 }
