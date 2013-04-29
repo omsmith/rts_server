84fdd8e18438b32c6065767f0afbfd5007115fc4
Josh Mackenzie <jnmacken@uwaterloo.ca>
2013-04-28 20:44:34 -0400

    Added my actualy complete and up to date work

--- src/server_battle/Unit_Information.java
+++ src/server_battle/Unit_Information.java
@@ -1,5 +1,35 @@
 package server_battle;
 
-public class Unit_Information {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+class Unit_Element{ 
+	public final Position posn; 
+	public final Integer unique_ID; 
+	public Unit_Element(Position Posn, Integer Unique_ID) { 
+	  this.posn = Posn; 
+	  this.unique_ID = Unique_ID; 
+	} 
+}
+
+class Unit_SortedList{
+	private List<Unit_Element> list = new ArrayList<Unit_Element>();
+	
+	public Unit_SortedList(){
+	}
+		
+	public boolean add(Position posn, String unit) {
+    int index = Collections.binarySearch(this, mt);
+    if (index < 0) index = ~index;
+    super.add(index, mt);
+    return true;
+    }
+}
+
+public class Unit_Information {
+	Map<Integer,String> unitTable=new HashMap<Integer, String>();
+	Map<Position,String> unitPositionTable=new HashMap<Position, String>();
 }
