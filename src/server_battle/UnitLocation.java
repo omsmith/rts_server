60adbb1ee9668977f95cf3e187f3701774b101a2
Andrew Davidson <aadavids@uwaterloo.ca>
2013-06-13 23:44:18 -0400

    implimenting mongo db interface

--- src/server_battle/UnitLocation.java
+++ src/server_battle/UnitLocation.java
@@ -2,6 +2,7 @@ package server_battle;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Set;
 
 import com.mongodb.Mongo;
@@ -16,6 +17,7 @@ import com.mongodb.DBCursor;
 import com.mongodb.ServerAddress;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UnitLocation {
 
@@ -25,23 +27,20 @@ public class UnitLocation {
 	public static final int port = 27017;
 	public static final String collectionName = "places";
 	Mongo mongo;
-	DBCollection coll;
+	DB db;
+	DBCollection units;
 	
-	void Init()	{
+	UnitLocation()	{
 		MongoClient mongoClient;
 		try {
 			mongoClient = new MongoClient("localhost");
-			DB db = mongoClient.getDB( "dbName" );
-		
-			coll = db.getCollection("unit_position");
-			coll.ensureIndex(new BasicDBObject("loc", "2d"), indexName);
-			    
-			BasicDBObject doc = new BasicDBObject("name", "MongoDB").
-	                append("type", "database").
-	                append("count", 1).
-	                append("info", new BasicDBObject("x", 203).append("y", 102));
-
-			coll.insert(doc);
+			db = mongoClient.getDB( "dbName" );
+			units = db.getCollection("unit_position");
+			//this.AddUnit(new AtomicInteger(), new Position(0, 0)); // puts a test unit at the origin
+			BasicDBObject index = new BasicDBObject();
+			index.put("loc","2d");
+			units.ensureIndex(index); // indexes as geospatialIdx
+			
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,24 +49,62 @@ public class UnitLocation {
 		
 	}
 	
-	// adds unit to map
-	void AddUnit() {
-		
+	// adds unit to map, should be functional
+	void AddUnit(final AtomicInteger uniqueID, Position unitPosition) {
+		BasicDBObject unit = new BasicDBObject("name", "MongoDB");
+		unit.put("uniqueID", "1105409");
+		BasicDBObject loc = new BasicDBObject();
+		loc.put ("x", unitPosition.getx());
+		loc.put("y", unitPosition.gety());
+		unit.put("loc", loc);
+		units.insert(unit);
 	}
 	
-	// update unit location on map
-	void UpdateUnit() {
+	// update unit location on map, TODO
+	void MoveUnit(final AtomicInteger uniqueID, Position newPosition) {
+		BasicDBObject query = new BasicDBObject("uniqueID", uniqueID);
+		BasicDBObject update = new BasicDBObject();
 
+		units.update(query, update);
+	
 	}
 	
-	// reads units at a given location/area 
-	void ReadUnits()	{
+	// get unit by unique id, should be functional
+	void GetUnit(final AtomicInteger uniqueID)	{
+		BasicDBObject query = new BasicDBObject("uniqueID", uniqueID);
+		DBCursor cursor = units.find(query);
 		
+		try {
+			while(cursor.hasNext()) {
+				DBObject result = cursor.next();
+				System.out.println(result); // for testing
+				
+			}
+		} finally {
+			   cursor.close();
+		}
+	}
+	
+	
+	ArrayList<AtomicInteger> FindUnitsNear(Position unitPosition)	{ // should be functional, TODO: by default returns nearest 50 units, will update 
+		BasicDBObject query = new BasicDBObject("$near", new double[] {unitPosition.getx(), unitPosition.gety()});
+		DBCursor cursor = units.find(query);
+		ArrayList<AtomicInteger> unitList = new ArrayList<AtomicInteger>();
+		try {
+		while(cursor.hasNext()) {
+			DBObject result = cursor.next();
+			System.out.println(result);
+			unitList.add((AtomicInteger) result.get("uniqueId"));
+	   }
+		} finally {
+		   cursor.close();
+		}
+		return unitList;
 	}
 	
 	// removes unit from map
 	void Destroy() {
-		
+		// TODO
 	}
 	
 }
